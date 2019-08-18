16c88813a0583933b98937bb71c6403aab9a6a3d
Dawn McNeil <dawn.a.mcneil@gmail.com>
2019-08-18 06:57:18 -0400

    convert wrapper to json for test match

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json'
 
 import Button from './button';
 
@@ -7,5 +8,6 @@ describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
         const wrapper = shallow(<Button />);
         wrapper.find('button').simulate('click');
+        expect(toJson(wrapper)).toMatchSnapshot()
     });
 });
